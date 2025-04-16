1e78e50253255d0eebe731e5b5d8b519fa47d1eb
joseph03 <joseph03sg@gmail.com>
2025-04-16 21:47:37 +0800

    test snyk workflow

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world-function"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "hello_world.lambda_handler"
+  runtime       = "python3.12"
+  filename      = "${path.module}/lambda/hello_world.zip"
+  
+  source_code_hash = filebase64sha256("${path.module}/lambda/hello_world.zip")
+}
