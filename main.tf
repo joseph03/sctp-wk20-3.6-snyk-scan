e200c13c648a2f1351c04ec727e0b9b8a9511a43
joseph03 <joseph03sg@gmail.com>
2025-04-17 17:54:51 +0800

    ignore node_modules

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "lambda_exec_role" {
       }
     ]
   }) 
-}
+} 
 
 resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_exec_role.name
